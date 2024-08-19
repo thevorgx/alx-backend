02f46661cc630ee422171019d6b753ac90cf5613
thevorgx <kh.elallali@gmail.com>
2024-08-20 00:18:29 +0100

    task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Simple helper function"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """function helper that return a tuple that contain
+    start idx and end idx"""
+    start_idx = (page-1) * page_size
+    end_idx = start_idx + page_size
+    idx_range = (start_idx, end_idx)
+    return (idx_range)
