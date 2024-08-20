96340a6d28b6a49986cdebcac984987e7e403576
thevorgx <kh.elallali@gmail.com>
2024-08-20 08:38:14 +0100

    task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""BasicCache Module"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        """put item on cache_data dict"""
+        if key is not None and item is not None:
+            self.cache_data.update({key: item})
+
+    def get(self, key):
+        """get key from cache_data dict"""
+        if key is None or key not in self.cache_data:
+            return (None)
+        the_key = self.cache_data.get(key)
+        return (the_key)
