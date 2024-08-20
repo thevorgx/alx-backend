10a254dcde0004fb5d676d0c1c1b9f8d7d9ba885
thevorgx <kh.elallali@gmail.com>
2024-08-20 12:02:27 +0100

    update fifo logic

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -9,11 +9,11 @@ class FIFOCache(BaseCaching):
         """put item on cache_data dict using FIFO ALGO"""
         if key is not None and item is not None:
             self.cache_data.update({key: item})
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            my_dict = self.cache_data
-            del_key = next(iter(my_dict))
-            my_dict.pop(del_key)
-            print(f"DISCARD: {del_key}")
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                my_dict = self.cache_data
+                del_key = next(iter(my_dict))
+                my_dict.pop(del_key)
+                print(f"DISCARD: {del_key}")
 
     def get(self, key):
         """get key from cache_data dict"""
