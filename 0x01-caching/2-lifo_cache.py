3deda83b928df5aa7f5843d5aa9a39f2ed63bcce
thevorgx <kh.elallali@gmail.com>
2024-08-21 12:32:45 +0100

    test

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -4,15 +4,22 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """LIFOCache class inherit from BaseCaching"""
+    """LIFOCache class that inherits from BaseCaching"""
+    
+    def __init__(self):
+        """Initialize the cache"""
+        super().__init__()
+        self.last_key = None
+
     def put(self, key, item):
-        """put item on cache_data dict using LIFO ALGO"""
+        """Put item into cache with LIFO behavior"""
         if key is not None and item is not None:
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                my_dict = self.cache_data
-                del_key, del_value = my_dict.popitem()
-                print(f"DISCARD: {del_key}")
-            self.cache_data.update({key: item})
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                if self.last_key is not None:
+                    print(f"DISCARD: {self.last_key}")
+                    del self.cache_data[self.last_key]
+            self.cache_data[key] = item
+            self.last_key = key
 
     def get(self, key):
         """get key from cache_data dict"""
