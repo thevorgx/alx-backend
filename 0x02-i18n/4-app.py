aa748d087a732180c35fb51488fc37f0e2a26096
thevorgx <kh.elallali@gmail.com>
2024-08-27 23:41:37 +0100

    machine change push

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,1 +1,32 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Basic Flask app"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config():
+    """Configuration class for the Flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """define a route and render HTML template"""
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """locale from request: best language match"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run()
