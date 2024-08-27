7459d5b4ed1c0ab9594e1bdce6eb8e1fcf9ac0bf
thevorgx <kh.elallali@gmail.com>
2024-08-27 22:29:01 +0100

    task 2 and some mini updates

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -19,7 +19,7 @@ babel = Babel(app)
 @app.route('/')
 def index():
     """define a route and render HTML template"""
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
